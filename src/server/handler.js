1b4435a5cdbb789b2e25eae226d594411f2a9acd
Albar Pambagio Arioseto <albarpambagio@gmail.com>
2024-05-20 14:28:44 +0700

    adjusted

--- src/server/handler.js
+++ src/server/handler.js
@@ -1,30 +1,35 @@
 const predictClassification = require('../services/inferenceService');
-const crypto = require('crypto');
- 
+const Boom = require('@hapi/boom');
+
 async function postPredictHandler(request, h) {
-  const { image } = request.payload;
-  const { model } = request.server.app;
- 
-  const { confidenceScore, label, explanation, suggestion } = await predictClassification(model, image);
-  const id = crypto.randomUUID();
-  const createdAt = new Date().toISOString();
- 
-  const data = {
-    "id": id,
-    "result": label,
-    "explanation": explanation,
-    "suggestion": suggestion,
-    "confidenceScore": confidenceScore,
-    "createdAt": createdAt
-  }
- 
-  const response = h.response({
-    status: 'success',
-    message: confidenceScore > 99 ? 'Model is predicted successfully.' : 'Model is predicted successfully but under threshold. Please use the correct picture',
-    data
-  })
-  response.code(201);
-  return response;
+    try {
+        const { image } = request.payload;
+        const { model } = request.server.app;
+
+        const { label, suggestion } = await predictClassification(model, image);
+        const id = crypto.randomUUID();
+        const createdAt = new Date().toISOString();
+
+        const data = {
+            id,
+            result: label,
+            suggestion,
+            createdAt
+        };
+
+        return h.response({
+            status: 'success',
+            message: 'Model is predicted successfully',
+            data
+        }).code(201);
+    } catch (error) {
+        if (Boom.isBoom(error)) {
+            return error;
+        }
+        
+        console.error('Prediction Error:', error);
+        return Boom.badRequest('Terjadi kesalahan dalam melakukan prediksi');
+    }
 }
- 
+
 module.exports = postPredictHandler;
